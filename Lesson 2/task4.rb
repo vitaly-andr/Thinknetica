1444c7e175c60fc31600d769f1e39efe6e5434ec
Vitaly Andrianov <vitaly.andr@gmail.com>
2024-05-30 16:39:45 +0300

    Lesson2

---
+++ Lesson 2/task4.rb
@@ -0,0 +1,19 @@
+# Строка с русским алфавитом
+alphabet = ('а'..'я').to_a
+alphabet.insert(6, 'ё')  # Добавляем "ё" после "е", так как в основном диапазоне "ё" нет
+
+# Массив гласных букв
+vowels = ['а', 'е', 'ё', 'и', 'о', 'у', 'ы', 'э', 'ю', 'я']
+
+# Хеш для хранения гласных и их индексов
+vowel_indices = {}
+
+# Заполнение хеша
+alphabet.each_with_index do |letter, index|
+  if vowels.include?(letter)
+    vowel_indices[letter] = index + 1  # Порядковый номер, начиная с 1
+  end
+end
+
+# Вывод хеша
+puts vowel_indices.inspect
---
+++ Lesson 2/task6.rb
@@ -0,0 +1,28 @@
+# Хеш для хранения данных о покупках
+cart = {}
+
+loop do
+  puts "Введите название товара или 'стоп' для завершения:"
+  title = gets.chomp
+  break if title == "стоп"
+
+  puts "Введите цену за единицу товара:"
+  price = gets.to_f
+
+  puts "Введите количество купленного товара:"
+  quantity = gets.to_f
+
+  # Добавляем информацию в хеш
+  cart[title] = {price: price, quantity: quantity}
+end
+
+total = 0
+
+puts "Список покупок:"
+cart.each do |title, info|
+  sum = info[:price] * info[:quantity]
+  puts "#{title}: #{info[:quantity]} шт. по #{info[:price]} руб. = #{sum} руб."
+  total += sum
+end
+
+puts "Итоговая сумма всех покупок: #{total} руб."
