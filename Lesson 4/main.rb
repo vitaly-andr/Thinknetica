7639ea937e235e250eeaae4db47bcb055bb131da
Vitaly Andrianov <vitaly.andr@gmail.com>
2024-06-22 01:29:44 +0300

    Lesson 9 Rubocop Refactoring Убрал все замечания RuboCop Благодаря лямбдам в частности в main.rb Самому понравилось Потом с остальными разберусь файлами

--- Lesson 4/main.rb
+++ Lesson 4/main.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require_relative 'managers/stations_manager'
 require_relative 'actions/station_actions'
 require_relative 'managers/trains_manager'
@@ -10,49 +12,59 @@ require_relative 'menu'
 require_relative 'helpers/ui_helpers'
 require_relative 'actions/seed_action'
 
-def main
-  stations_manager = StationsManager.new
-  trains_manager = TrainsManager.new
-  routes_manager = RoutesManager.new
-  rail_car_manager = RailCarManager.new
-  seed_action = SeedAction.new
-  main_menu = Menu.new(
-    {
-      1 => 'Управление станциями',
-      2 => 'Управление поездами',
-      3 => 'Управление маршрутами',
-      4 => 'Управление вагонами',
-      5 => 'Просмотреть список станций и список поездов на станции',
-      100 => 'Засеять демо данными',
-      0 => 'Выход'
+# Инициализируем менеджеров
+stations_manager = StationsManager.new
+trains_manager = TrainsManager.new
+routes_manager = RoutesManager.new
+rail_car_manager = RailCarManager.new
+seed_action = SeedAction.new
 
-    }
-  )
+# Создаем хэш действий как константу
+ACTIONS = {
+  1 => -> { station_actions(stations_manager) },
+  2 => -> { train_actions(trains_manager, rail_car_manager, routes_manager) },
+  3 => -> { route_actions(routes_manager, stations_manager) },
+  4 => -> { rail_car_actions(rail_car_manager) },
+  5 => -> { stations_manager.list_stations_and_trains },
+  100 => -> { seed_action.seed(trains_manager, rail_car_manager, routes_manager, stations_manager) },
+  0 => -> { false }
+}.freeze
+
+# Создаем меню как константу
+MAIN_MENU = Menu.new(
+  {
+    1 => 'Управление станциями',
+    2 => 'Управление поездами',
+    3 => 'Управление маршрутами',
+    4 => 'Управление вагонами',
+    5 => 'Просмотреть список станций и список поездов на станции',
+    100 => 'Засеять демо данными',
+    0 => 'Выход'
+  }
+).freeze
+
+def process_menu_choice(choice)
+  action = ACTIONS[choice]
+  if action
+    action.call
+  else
+    puts UIHelpers.red('Something went wrong!')
+  end
+  true
+end
+
+def main_loop
   loop do
-    main_menu.display
-    choice = main_menu.get_choice
+    MAIN_MENU.display
+    choice = MAIN_MENU.get_choice
     next if choice.nil?
 
-    case choice
-
-    when 1
-      station_actions(stations_manager)
-    when 2
-      train_actions(trains_manager, rail_car_manager, routes_manager)
-    when 3
-      route_actions(routes_manager, stations_manager)
-    when 4
-      rail_car_actions(rail_car_manager)
-    when 5
-      stations_manager.list_stations_and_trains
-    when 100
-      seed_action.seed(trains_manager, rail_car_manager, routes_manager, stations_manager)
-    when 0
-      break
-    else
-      puts UIHelpers.red('Something went wrong!')
-    end
+    break unless process_menu_choice(choice)
   end
 end
 
-main if __FILE__ == $0
+def main
+  main_loop
+end
+
+main if __FILE__ == $PROGRAM_NAME
